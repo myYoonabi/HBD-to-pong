23285354b45a56aae8dac09972ba42eb231a9e7a
myYoonabi <Amornwit0531@gmail.com>
2025-02-19 18:01:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const message = document.querySelector('.message');
+
+message.addEventListener('click', () => {
+    message.textContent = "Wishing you a day filled with love, joy, and everything that makes you happy. ";
+    message.style.color = "#ff1493";
+    message.style.fontSize = "28px";
+});
